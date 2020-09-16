713dbb99cd2bc4ceaff913e216700f354d9f4ee9
evilpaprika <ivil.paprika@gmail.com>
2020-09-16 13:28:03 +0500

    footer template

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import matter from 'gray-matter';
+import React from 'react';
+
+
+export default function Footer() {
+    return (
+        <>
+
+            <style jsx>
+                {`
+
+                `}
+            </style>
+        </>
+    );
+}
