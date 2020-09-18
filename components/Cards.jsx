0ce49e7d856519e9e71edacb976ae90033f79e1c
evilpaprika <ivil.paprika@gmail.com>
2020-09-18 20:21:26 +0500

    Update Cards.jsx

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,25 +1,53 @@
 import matter from 'gray-matter';
-import Link from 'next/link';
 import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Media from 'react-bootstrap/Media';
-import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 import landingPageContent from '../content/landing-page.md';
 
 
 export default function Cards() {
     const { cards } = matter(landingPageContent).data;
-    console.log('cards:', cards);
 
     return (
-        <div>
-
-            <style jsx>
+        <section className="mt-5">
+            <CardGroup className="justify-content-around">
+                {cards.map(({ picture, title, caption, link_slug }) => {
+                    return (
+                        <Card
+                            key={link_slug}
+                            border="primary"
+                            className="rounded shadow mb-4 border-left"
+                            style={{ width: '18rem', flex: 'initial' }}
+                        >
+                            <a href={`/templated-page/${link_slug}`}>
+                                <Card.Img className="cardImage" variant="top" src={picture} />
+                                <Card.Body>
+                                    <Card.Title>{title}</Card.Title>
+                                    <Card.Text>
+                                        {caption}
+                                    </Card.Text>
+                                </Card.Body>
+                            </a>
+                        </Card>
+                    );
+                })}
+                <div style={{ width: '18rem', flex: 'initial' }} />
+                <div style={{ width: '18rem', flex: 'initial' }} />
+                <div style={{ width: '18rem', flex: 'initial' }} />
+            </CardGroup>
+            <style global jsx>
                 {`
-
+                    .cardImage {
+                        width: 100%;
+                        height: 15vw;
+                        object-fit: cover;
+                    }
+                    .cardImage__last {
+                        margin-right:auto;
+                    }
                 `}
             </style>
-        </div>
+        </section>
     );
 }
