ef8248daa013afaa4e484e3b240e727782f3afec
evilpaprika <ivil.paprika@gmail.com>
2020-09-20 00:15:39 +0500

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,7 @@ import matter from 'gray-matter';
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Media from 'react-bootstrap/Media';
 import Row from 'react-bootstrap/Row';
 
 import footerContent from '../content/footer.md';
@@ -16,28 +17,40 @@ export default function Footer() {
             <footer className="footer mb-5">
                 <Container>
                     <Row>
-                        <Col className="justify-content-between text-light">
+                        <Col className="justify-content-between text-light d-md-flex align-items-center d-none">
                             <img
                                 width={100}
                                 height={100}
                                 src="/static/logo-white.svg"
                                 alt="site logo"
                             />
+                            <Media.Body className="text-white">
+                                <h1 className="footer__title text-left ml-3">
+                                    <div className="footer__title_first_word">МАКСИМАЛЬНАЯ</div>
+                                    ЗАЩИТА ПЕРИМЕТРА
+                                </h1>
+                            </Media.Body>
                         </Col>
                         <Col className="justify-content-between text-light">
-                            <Row>
+                            <Row className="info_row">
                                 <span className="info__label">Адрес: </span>
                                 <span className="info__data">{physical_address}</span>
                             </Row>
-                            <Row>
+                            <Row className="info_row">
                                 <span className="info__label">Телефон: </span>
                                 <a className="info__data" href={`tel:${phone_number}`}>
                                     {phone_number}
                                 </a>
                             </Row>
-                            <Row>
+                            <Row className="info_row">
                                 <span className="info__label">E-Mail: </span>
-                                <a href={`mailto:${email}`} className="info__data">{email}</a>
+                                <a
+                                    href={`mailto:${email}`}
+                                    className="info__data"
+                                    rel="noreferrer"
+                                    target="_blank"
+                                >{email}
+                                </a>
                             </Row>
                         </Col>
                     </Row>
@@ -45,20 +58,33 @@ export default function Footer() {
             </footer>
             <style jsx>
                 {`
+                    @media (max-width: 576px) {
+                        :global(.footer__logo) {
+                            display: none;
+                        }
+                    }
+
+                    .footer__title {
+                        font-size: 25px;
+                    }
+                    .footer__title_first_word {
+                        font-size: 30px;
+                    }
+                    :global(.info_row) {
+                        align-items: baseline;
+                        flex-wrap: nowrap;
+                    }
                     .info__label {
                       margin-left: 10px;
                       font-size: 18px;
                       font-weight: bold;
-                      line-height: 1.5;
-
+                      margin-right: 5px;
                     }
                     .info__data {
                         color: #fff;
                         font-size: 16px;
-                        line-height: 1.5;
                     }
                     .footer {
-
                         position: absolute;
                         bottom: 0;
                         width: 100%;
